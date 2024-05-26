6e55073e4b192e14cf1336a834a093039ac76736
pavansweb <155568268+pavansweb@users.noreply.github.com>
2024-05-26 10:57:25 +0000

    Files removed
    removed unnesecary images and files

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('enterButton').addEventListener('click', function() {
-    document.getElementById('rose1').classList.add('grow');
-    document.getElementById('rose2').classList.add('grow');
-    document.getElementById('rose3').classList.add('grow');
-});
