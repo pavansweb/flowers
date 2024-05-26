a25626cd2190db8eaf5c260f786c8a6629046a3c
pavansweb <155568268+pavansweb@users.noreply.github.com>
2024-05-26 10:55:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('enterButton').addEventListener('click', function() {
+    document.getElementById('rose1').classList.add('grow');
+    document.getElementById('rose2').classList.add('grow');
+    document.getElementById('rose3').classList.add('grow');
+});
